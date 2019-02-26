daed0074e743f7b75a139d37775cb0443038fd57
Manuel Marinez <marinezdev@gmail.com>
2019-02-26 15:12:59 -0400

    Add allure report

--- testcases/test_viceland.py
+++ testcases/test_viceland.py
@@ -1,18 +1,21 @@
 import time
 import unittest
 
+import allure
 import pytest
 
 
 @pytest.mark.usefixtures("setup")
 class Vice(unittest.TestCase):
 
+    @allure.step("Validate label step")
     def test_validate_vice_label(self):
         _signin_label = self.driver.find_element_by_xpath("(//a[text()='Sign In'])[1]")
 
         if _signin_label.is_displayed():
             assert True
 
+    @allure.step("Validate channel label step")
     def test_check_episode_access(self):
         episode_channel = self.driver.find_element_by_xpath(
             "(//a[@class='grid__wrapper__card grd-col col-12-xs col-6-m col-4-xl'])[1]")
@@ -20,6 +23,7 @@ class Vice(unittest.TestCase):
         if episode_channel.is_displayed():
             assert True
 
+    @allure.step("Ensure channel click access step")
     def test_episode_access(self):
         episode_channel = self.driver.find_element_by_xpath(
             "(//a[@class='grid__wrapper__card grd-col col-12-xs col-6-m col-4-xl'])[1]")
