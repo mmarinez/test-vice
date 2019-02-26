9a1fd39c9c37b9312b002d79aeb147918d33305c
Manuel Marinez <marinezdev@gmail.com>
2019-02-26 13:55:49 -0400

    Restore project

---
+++ conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--driver", action="store", default="chrome", help="Type in browser type (e.g. chrome)")
+    parser.addoption("--headless", action="store", default="headless", help="Is headless driver?")
+
+
+@pytest.yield_fixture(scope="class", autouse=True)
+def setup(request):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.viceland.com/en_us")
+    request.cls.driver = driver
+
+    yield driver
+    driver.quit()
---
+++ testcases/test_viceland.py
@@ -0,0 +1,31 @@
+import time
+import unittest
+
+import pytest
+
+
+@pytest.mark.usefixtures("setup")
+class Vice(unittest.TestCase):
+
+    def test_validate_vice_label(self):
+        _signin_label = self.driver.find_element_by_xpath("(//a[text()='Sign In'])[1]")
+
+        if _signin_label.is_displayed():
+            assert True
+
+    def test_check_episode_access(self):
+        episode_channel = self.driver.find_element_by_xpath(
+            "(//a[@class='grid__wrapper__card grd-col col-12-xs col-6-m col-4-xl'])[1]")
+
+        if episode_channel.is_displayed():
+            assert True
+
+    def test_episode_access(self):
+        episode_channel = self.driver.find_element_by_xpath(
+            "(//a[@class='grid__wrapper__card grd-col col-12-xs col-6-m col-4-xl'])[1]")
+        episode_channel.click()
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
