8396a9403c36585fe1643ec2fb62e1d331dc726f
Manuel Marinez <marinezdev@gmail.com>
2019-03-11 08:17:53 -0400

    Add Environment variables

--- pageobject/vicelandpage.py
+++ pageobject/vicelandpage.py
@@ -16,8 +16,7 @@ class vicelandPage(Base):
     _iframe_player = (By.XPATH, "//iframe[@class='player-embed']")
     _volume_control = (By.XPATH, "//div[contains(@class,'vp__controls__icon vp__icon--volume')]")
     _player_wrapper = (By.XPATH, "//div[contains(@class,'vp__container vp__container--')]")
-
-    os.environ['VICELAND'] = "https://www.viceland.com/en_us"
+ 
     VICELAND_URL = os.environ.get('VICELAND')
 
     @property
