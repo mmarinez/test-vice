fb9da258ba1df65b2c227065e502b390df35b1b5
Manuel Marinez <marinezdev@gmail.com>
2019-03-25 15:28:06 -0400

    Add refactor to free test scenario

--- pageobject/freevicepage.py
+++ pageobject/freevicepage.py
@@ -13,10 +13,10 @@ import allure
 
 class freevicepage(Base):
 
-    _element_list = (By.CSS_SELECTOR, "div[class*=thumbnail],div[class*=container]")
+    _element_list = (By.CSS_SELECTOR, "div[class*=thumbnail] *, div[class*=lede] *")
     _section_headers = (By.CSS_SELECTOR, "h3,h2[class*='head']")
     _video_strings_validations = (
-        "video", "Video", "Episode", "Clip", "duration")
+        "video", "videos", "episodes", "clip", "duration", "shows")
 
     VICE_FREE = os.environ.get('VICE_FREE')
 
@@ -39,14 +39,6 @@ class freevicepage(Base):
         except:
             Driver.redirect_to("https://free.vice.com/en_us")
 
-    def catch_all_element_attributes(self, element):
-        attrs = Driver.driver.execute_script(
-            'var items = {}; ' +
-            'for (index = 0; index < arguments[0].attributes.length; ++index)' +
-            '{ items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value };' +
-            'return items;', element)
-        return attrs
-
     def is_videos_label_displayed(self):
         with allure.step("Validate that the video label is displayed"):
             for element in self.article_list:
@@ -54,16 +46,11 @@ class freevicepage(Base):
                 Driver.driver.execute_script(
                     'arguments[0].scrollIntoView()', element)
 
-                for element_attribute in self.catch_all_element_attributes(element):
-                    if not any(validator in element.get_attribute(element_attribute)
-                               for validator in self._video_strings_validations):
+                if any(validator in element.get_attribute("class") for validator in self._video_strings_validations):
 
-                        if element.text != self.article_list[-1].text:
-                            continue
-                        return True
-                    else:
-                        print(element.get_attribute(element_attribute))
-                        return False
+                    print(element.get_attribute("class"))
+                    return False
+            return True
 
     def is_video_text_assigned(self):
         with allure.step("Validate that the headers dont have video text"):
@@ -72,16 +59,11 @@ class freevicepage(Base):
                 Driver.driver.execute_script(
                     'arguments[0].scrollIntoView()', header)
 
-                for element_attribute in self.catch_all_element_attributes(header):
-                    if not any(validator in header.get_attribute(element_attribute)
-                               for validator in self._video_strings_validations):
+                if any(validator in header.text.lower() for validator in self._video_strings_validations):
 
-                        if header.text != self.section_headers[-1].text:
-                            continue
-                        return True
-                    else:
-                        print(header.get_attribute(element_attribute))
-                        return False
+                    print(header.text)
+                    return False
+            return True
 
     def has_video_player(self):
         with allure.step("Validate that video player free"):
